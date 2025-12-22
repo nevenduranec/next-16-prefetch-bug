2bbff75bb334382ca433729c262d30c077410728
Robin Clowers <robin.clowers@gmail.com>
2025-12-22 12:01:28 -0800

    More pages and links

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,9 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Welcome
           </h1>
-          <Link href={"/test"}>View page</Link>
+          <Link href={"/test_slug"}>View slug</Link>
+          <Link href={"/test_slug"}>View test slug</Link>
+          <Link href={"/test_slug/test_page"}>View test page</Link>
         </div>
       </main>
     </div>
