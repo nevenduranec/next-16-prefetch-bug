0579907a9181416b85eed7c212a85b2dfe35ec5d
Robin Clowers <robin.clowers@gmail.com>
2025-12-22 12:18:06 -0800

    Add basePath

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: '/app',
   async rewrites() {
     return {
       fallback: [
         {
+          basePath: false,
           source: '/:path*',
           destination: 'https://example.com/:path*',
         },
