a495edb899340360227d368c426fe9f266adda26
Robin Clowers <robin.clowers@gmail.com>
2025-12-22 12:13:21 -0800

    Add fallback

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: 'https://example.com/:path*',
+        },
+      ],
+    };
+  },
 };
+
 
 export default nextConfig;
