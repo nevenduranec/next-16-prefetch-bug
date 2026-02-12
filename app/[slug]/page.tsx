63052cf7d293c6a72169ea87b9b8b1ea09ea88a6
Neven Đuranec <neven.duranec@gmail.com>
2026-02-12 09:00:11 +0100

    Add generateStaticParams

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link"
 
+export function generateStaticParams() {
+  return [{ slug: "test_slug" }]
+}
+
 export default function SlugPage() {
   return (
     <>
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
         Welcome
       </h1>
-      <Link href={"/test_slug"}>View slug</Link>
-
+      <Link href={"/test_slug"}>View slug (SSG)</Link>
+      <Link href={"/test_slug_2"}>View slug 2</Link>
     </>
   );
 }
